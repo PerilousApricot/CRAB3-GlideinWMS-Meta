ae07c3e0734df2da2e7c78fd7ddfa5039bec8287
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2013-10-13 08:14:04 -0400

    Updating README

---
+++ scripts/copy-credentials-to-vm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Move the developer's credentials from ~ on the host to ~ in the VM
+OPTIONS=$(vagrant ssh-config | awk -v ORS=' ' '{print "-o " $1 "=" $2}')
+if [[ -d ~/.globus ]]; then
+    echo "Moving ~/.globus into VM"
+    scp -r ${OPTIONS} ~/.globus localhost:~
+else
+    echo "Not moving ~/.globus (not found)"
+fi
+
+ssh ${OPTIONS} localhost mkdir -p /home/vagrant/.ssh
+for KEY in ~/.ssh/*.pub; do
+    if [[ -f $KEY ]]; then
+        echo "Moving $KEY into VM"
+        scp ${OPTIONS} $KEY localhost:/home/vagrant/.ssh/$(basename $KEY)
+    fi
+done
